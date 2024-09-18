409110e60008acf3045b725c007a28b8019928d8
Chris <chris@seniorcodereviewbuddy.com>
2024-09-18 10:48:05 -0400

    Remove podcast string from move_file.py (#7)
    podcast was only used to determine the complete archive path, but it's
    cleaner to have the caller compute the archive path and just pass it
    indirectly.
    
    Also change destination to file_destination and pass in the full path to
    be consistent with how the file and archive location are passed in.

--- move_file.py
+++ move_file.py
@@ -9,51 +9,49 @@ import helper
 
 
 def _MoveFileOverImpl(
-    podcast: str,
-    file: pathlib.Path,
-    destination: pathlib.Path,
-    archive_folder: pathlib.Path,
+    file_source: pathlib.Path,
+    file_destination: pathlib.Path,
+    archive_destination: pathlib.Path,
     title_prefix: str,
     speed: float,
     dry_run: bool,
 ) -> None:
     # Make copies of important episodes.
-    if archive_folder:
+    if archive_destination:
         if dry_run:
-            print("Dry run, would have archived %s" % (file))
+            print("Dry run, would have archived %s" % (file_source))
         else:
-            podcast_archive_folder = pathlib.Path(archive_folder, podcast)
-            os.makedirs(podcast_archive_folder, exist_ok=True)
-            filename = file.name
-            archive_destination = pathlib.Path(podcast_archive_folder, filename)
-            print("Making copy of %s in archive (%s)" % (filename, archive_destination))
-            shutil.copyfile(file, archive_destination)
+            os.makedirs(archive_destination.parent, exist_ok=True)
+            print(
+                "Making copy of %s in archive (%s)"
+                % (file_source.name, archive_destination)
+            )
+            shutil.copyfile(file_source, archive_destination)
 
-    dest = pathlib.Path(destination, file.name)
-    album = file.parent.name
+    album = file_source.parent.name
     if dry_run:
-        print("Dry run, would have moved %s to %s" % (file, dest))
+        print("Dry run, would have moved %s to %s" % (file_source, file_destination))
         print("With album %s" % album)
     else:
-        helper.PrepareAudioAndMove(file, dest, album, title_prefix, speed)
+        helper.PrepareAudioAndMove(
+            file_source, file_destination, album, title_prefix, speed
+        )
 
 
 def main(args: typing.Optional[typing.List[str]]) -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--podcast-show-name", type=str, required=True)
     parser.add_argument("--file-path", type=pathlib.Path, required=True)
-    parser.add_argument("--destination", type=pathlib.Path, required=True)
-    parser.add_argument("--archive-folder", type=pathlib.Path, default=None)
+    parser.add_argument("--file-destination", type=pathlib.Path, required=True)
+    parser.add_argument("--archive-destination", type=pathlib.Path, default=None)
     parser.add_argument("--title-prefix", type=str, default="")
     parser.add_argument("--speed", type=float, default=1.0)
     parser.add_argument("--dry-run", action="store_true", default=False)
     parsed_args = parser.parse_args(args)
 
     _MoveFileOverImpl(
-        parsed_args.podcast_show_name,
         parsed_args.file_path,
-        parsed_args.destination,
-        parsed_args.archive_folder,
+        parsed_args.file_destination,
+        parsed_args.archive_destination,
         parsed_args.title_prefix,
         parsed_args.speed,
         parsed_args.dry_run,
