ed4621319037788dac10d329e3114d52a282ecf3
Chris Sharp <chris@seniorcodereviewbuddy.com>
2024-09-20 16:40:04 -0400

    Move time_helper from helper to time_helper
    No functionality changes, although some new tests were added.

--- helper.py
+++ helper.py
@@ -7,27 +7,6 @@ import audio_metadata
 import conversions
 
 
-def SecondsToString(seconds: float) -> str:
-    days, remainder = divmod(seconds, 3600 * 24)
-    hours, remainder = divmod(remainder, 3600)
-    minutes, seconds = divmod(remainder, 60)
-
-    duration_string = ""
-    if days > 0:
-        duration_string += "%dd" % days
-    if hours > 0:
-        duration_string += "%dh" % hours
-    if minutes > 0:
-        duration_string += "%dm" % minutes
-    if seconds > 0:
-        duration_string += "%ds" % seconds
-
-    if not duration_string:
-        duration_string = "0s"
-
-    return duration_string
-
-
 def _GenerateTitle(file: pathlib.Path, title_prefix: str) -> str:
     current_title = audio_metadata.GetTitle(file)
     if current_title:
