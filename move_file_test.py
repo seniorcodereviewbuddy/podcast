fcbd2940a02e4c3b2ee1116b5cf4cfcdf62a3c70
Chris Sharp <chris@seniorcodereviewbuddy.com>
2024-09-20 18:24:42 -0400

    Determine Title before starting subprocess to update audio files
    Move title generation to ProcessAndMoveFilesOver and pass in title and album.
    
    This means the spawned process isn't making any decisions about the file value
    and instead is only worried about implementing them.

--- move_file_test.py
+++ move_file_test.py
@@ -1,8 +1,10 @@
 import os
+import pathlib
 import shutil
 import tempfile
 import unittest
 
+import audio_metadata
 import move_file
 import test_utils
 
@@ -11,23 +13,23 @@ class TestMoveFile(unittest.TestCase):
     def setUp(self) -> None:
         self.holding_dir = tempfile.TemporaryDirectory()
 
-        test_file_source = os.path.join(
+        test_file_source = pathlib.Path(
             test_utils.TEST_DATA_DIR, test_utils.MP3_TEST_FILE
         )
-        self.podcast_file = os.path.join(
+        self.podcast_file = pathlib.Path(
             self.holding_dir.name, test_utils.MP3_TEST_FILE
         )
         shutil.copyfile(test_file_source, self.podcast_file)
 
-        self.destination = os.path.join(self.holding_dir.name, "destination")
+        self.destination = pathlib.Path(self.holding_dir.name, "destination")
         os.makedirs(self.destination)
 
-        self.archive = os.path.join(self.holding_dir.name, "archive")
+        self.archive = pathlib.Path(self.holding_dir.name, "archive")
 
-        self.destination_podcast_path = os.path.join(
+        self.destination_podcast_path = pathlib.Path(
             self.destination, os.path.basename(self.podcast_file)
         )
-        self.archived_podcast_path = os.path.join(
+        self.archived_podcast_path = pathlib.Path(
             self.archive, "fake_show_archive", os.path.basename(self.podcast_file)
         )
 
@@ -38,11 +40,15 @@ class TestMoveFile(unittest.TestCase):
         args = [
             "--dry-run",
             "--archive-destination",
-            self.archived_podcast_path,
+            str(self.archived_podcast_path),
             "--file-path",
-            self.podcast_file,
+            str(self.podcast_file),
             "--file-destination",
-            self.destination_podcast_path,
+            str(self.destination_podcast_path),
+            "--title",
+            "new_title",
+            "--album",
+            "new_album",
         ]
         move_file.main(args)
         self.assertTrue(os.path.isfile(self.podcast_file))
@@ -53,9 +59,13 @@ class TestMoveFile(unittest.TestCase):
         args = [
             "--dry-run",
             "--file-path",
-            self.podcast_file,
+            str(self.podcast_file),
             "--file-destination",
-            self.destination_podcast_path,
+            str(self.destination_podcast_path),
+            "--title",
+            "new_title",
+            "--album",
+            "new_album",
         ]
         move_file.main(args)
         self.assertTrue(os.path.isfile(self.podcast_file))
@@ -65,28 +75,50 @@ class TestMoveFile(unittest.TestCase):
     def testProdRunArchive(self) -> None:
         args = [
             "--archive-destination",
-            self.archived_podcast_path,
+            str(self.archived_podcast_path),
             "--file-path",
-            self.podcast_file,
+            str(self.podcast_file),
             "--file-destination",
-            self.destination_podcast_path,
+            str(self.destination_podcast_path),
+            "--title",
+            "new_title",
+            "--album",
+            "new_album",
         ]
         move_file.main(args)
         self.assertFalse(os.path.isfile(self.podcast_file))
         self.assertTrue(os.path.isfile(self.destination_podcast_path))
         self.assertTrue(os.path.isfile(self.archived_podcast_path))
 
+        self.assertEqual(
+            "new_title", audio_metadata.GetTitle(self.destination_podcast_path)
+        )
+        self.assertEqual(
+            "new_album", audio_metadata.GetAlbum(self.destination_podcast_path)
+        )
+
     def testProdRunNoArchive(self) -> None:
         args = [
             "--file-path",
-            self.podcast_file,
+            str(self.podcast_file),
             "--file-destination",
-            self.destination_podcast_path,
+            str(self.destination_podcast_path),
+            "--title",
+            "new_title",
+            "--album",
+            "new_album",
         ]
         move_file.main(args)
         self.assertFalse(os.path.isfile(self.podcast_file))
         self.assertTrue(os.path.isfile(self.destination_podcast_path))
         self.assertFalse(os.path.isfile(self.archived_podcast_path))
+
+        self.assertEqual(
+            "new_title", audio_metadata.GetTitle(self.destination_podcast_path)
+        )
+        self.assertEqual(
+            "new_album", audio_metadata.GetAlbum(self.destination_podcast_path)
+        )
 
 
 if __name__ == "__main__":
