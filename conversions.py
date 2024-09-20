597e680cf69f3721f980e2f7156c4f6ab6320221
Chris <chris@seniorcodereviewbuddy.com>
2024-09-20 16:12:18 -0400

    Create a new ConvertMatchingFileTypesInFolder function to handle all conversions (#21)
    Remove the old functions that had hard-coded conversions.
    
    Add tests
    
    FIXES #18

--- conversions.py
+++ conversions.py
@@ -7,24 +7,14 @@ import ffmpeg_helper
 LOUDNESS_TARGET = -10.0
 
 
-# TODO: Add tests, and make one function that just takes the pattern of files to convert.
-def ConvertAllMP4ToMP3(folder: pathlib.Path) -> None:
-    # Convert all mp4 to mp3
+def ConvertMatchingFileTypesInFolder(
+    folder: pathlib.Path, input_file_type: str, output_file_type: str
+) -> None:
     for file in folder.iterdir():
         ext = file.suffix.lower()
-        if ext.lower() == ".mp4":
-            ffmpeg_helper.ConvertFile(file, file.with_suffix(".mp3"))
-            # No need to keep the original file around now
-            os.remove(file)
-
-
-def ConvertAllWEBMToMP3(folder: pathlib.Path) -> None:
-    # Convert all webm to mp3
-    for file in folder.iterdir():
-        ext = file.suffix.lower()
-        if ext.lower() == ".webm":
-            ffmpeg_helper.ConvertFile(file, file.with_suffix(".mp3"))
-            # No need to keep the original file around now
+        if ext.lower() == input_file_type:
+            ffmpeg_helper.ConvertFile(file, file.with_suffix(output_file_type))
+            # No need to keep the original file around now.
             os.remove(file)
 
 
